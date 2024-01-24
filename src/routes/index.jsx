b481e160bee49d52dc7f1f8b03e59d2377b596e6
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-01-24 17:09:38 +0400

    update tutorial

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "./root";
+import Root, { loader as rootLoader, action as rootAction } from "./root";
 import ErrorPage from "./error-page";
-import Contact from "./contacts";
+import Contact, { loader as contactLoader } from "./contact";
+import EditContact, { action as editAction } from "./edit";
+import { action as destroyAction } from "./destroy";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
       errorElement: <ErrorPage />,
+      loader: rootLoader,
+      action: rootAction,
       children: [
         {
           path: "contacts/:contactId",
           element: <Contact />,
+          loader: contactLoader,
+        },
+        {
+          path: "contacts/:contactId/edit",
+          element: <EditContact />,
+          loader: contactLoader,
+          action: editAction,
+        },
+        {
+          path: "contacts/:contactId/destroy",
+          action: destroyAction,
+          errorElement: <div>Oops! There was an error.</div>,
         },
       ],
-  
     },
 ]);
 
