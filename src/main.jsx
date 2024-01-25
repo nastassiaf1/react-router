f118285a313f2a3fb77d103565216aa455bfba84
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-01-25 12:52:48 +0400

    update tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import router from "./routes";
+import router from "./router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,36 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
-import Root, { loader as rootLoader, action as rootAction } from "./root";
-import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader } from "./contact";
-import EditContact, { action as editAction } from "./edit";
-import { action as destroyAction } from "./destroy";
-
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      loader: rootLoader,
-      action: rootAction,
-      children: [
-        {
-          path: "contacts/:contactId",
-          element: <Contact />,
-          loader: contactLoader,
-        },
-        {
-          path: "contacts/:contactId/edit",
-          element: <EditContact />,
-          loader: contactLoader,
-          action: editAction,
-        },
-        {
-          path: "contacts/:contactId/destroy",
-          action: destroyAction,
-          errorElement: <div>Oops! There was an error.</div>,
-        },
-      ],
-    },
-]);
-
-export default router;
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">
+          the docs at reactrouter.com
+        </a>
+        .
+      </p>
+    );
+}
